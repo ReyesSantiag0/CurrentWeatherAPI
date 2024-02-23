4e0b6131c3b8b2012f743d276181aaac9aa78662
Santiago <practicantesena5@ticsocial.com.co>
2024-02-23 15:53:06 -0500

    Added metre forecasting API

--- src/helpers/apiOpenWeather.js
+++ src/helpers/apiOpenWeather.js
@@ -46,3 +46,25 @@ export const petitionWeatherCity = async (latCity, lonCity) => {
     console.error("Error en la solicitud:", error.message);
   }
 };
+
+export const petitionForecastCity = async (latCity, lonCity) => {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latCity}&lon=${lonCity}&units=metric&cnt=6&appid=${appid}&lang=es`;
+  const resp = await fetch(url);
+  try {
+    const data = await resp.json();
+    const cityWeatherDataForecast = data.list.map((results) => ({
+      dt: results.dt,
+      temp: results.main.temp,
+      country: data.city.country,
+      name: data.city.name,
+      dt_txt: results.dt_txt,
+      description: results.weather[0].description,
+      icon: results.weather[0].icon,
+    }));
+
+    console.log("cityWeatherDataForecast ", cityWeatherDataForecast);
+    return cityWeatherDataForecast;
+  } catch (error) {
+    console.error("Error en la solicitud:", error.message);
+  }
+};
